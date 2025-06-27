4795da157b8199576af98b8cb6b895057d98b958
tr <tr@scientific-wave.com>
2025-06-27 16:50:19 +0200

    minimum version

--- treffpunktfinder.js
+++ treffpunktfinder.js
@@ -1,4 +1,11 @@
 export function findeTreffpunkt(inputKoordinaten) {
     console.log('Suche treffpunkt zu input koordinaten: ' + inputKoordinaten);
-  return [51.5, 10.30];
+  let lats = 0;
+  let lons = 0;
+  const l = inputKoordinaten.length;
+  for(let i=0; i < l; i++) {
+    lats += inputKoordinaten[i][0];
+    lons += inputKoordinaten[i][1];
+  }
+  return [lats/l, lons/l];
 }
