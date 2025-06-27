f7afb6a6be9ee318353d542857851ddc98b4adaa
volkerrehberg <155998534+volkerrehberg@users.noreply.github.com>
2025-06-27 16:52:45 +0200

    ersten klick entfernen

--- map-logic.js
+++ map-logic.js
@@ -17,8 +17,8 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-const marker = L.marker([51.5, 10.09]).addTo(map)
-     .bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
+// const marker = L.marker([51.5, 10.09]).addTo(map)
+//      .bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
 
 // const circle = L.circle([51.508, 10.10], {
 //     color: 'red',
